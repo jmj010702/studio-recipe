4b979c08c1f2b7b4c7a3e9a708938f327fad7ca0
yoon <stay_on_track@naver.com>
2025-10-26 17:18:13 +0900

    프론트 참고용 코드 작성

--- recommended-recipe/src/page/App.jsx
+++ recommended-recipe/src/page/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Test from "../components/test";
+import SignupPage from "../SignupPage.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Test />}></Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+      // <Routes>
+        // <Route path="/" element={<Test />}></Route>
+      // </Routes>
+    // </BrowserRouter>
+    <SignupPage />
   );
 };
 
