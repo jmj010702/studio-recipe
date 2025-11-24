0d5a8e84f577e9cb04e3e7c9e340cb4703b70149
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2025-11-24 09:11:40 +0900

    Update clean_old_images.sh

--- recipe/scripts/clean_old_images.sh
+++ recipe/scripts/clean_old_images.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-# 이전 이미지들을 정리하여 디스크 공간을 확보합니다.
-echo "Cleaning up old Docker images..."
+set -eux 
+
+echo "Cleaning up dangling Docker images and unused layers..."
+# docker image prune -f 는 dangling images만 지움.
+# docker system prune -f -a 는 dangling images & unused images & stopped containers & unused volumes
+# 그러나 ASG에서 실행되므로, 필요한 것만 지우는 것이 좋음.
+# dangling images (태그 없이 남은 이미지)만 지우는 것이 목적이라면 prune -f 그대로.
+# 모든 사용하지 않는 이미지를 지우려면 'docker image prune -a -f'
+
 docker image prune -f
+
+echo "Finished cleaning old Docker images.
